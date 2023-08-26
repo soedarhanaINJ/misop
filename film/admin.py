9f9ac758fad34ab0739e58b8e75765d7987b4995
soedarhanaINJ <i.soedarhana@gmail.com>
2023-08-27 01:59:26 +0900

    Register models

--- film/admin.py
+++ film/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Posting, Category
 
-# Register your models here.
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Category, AdminCategory)
+
+class AdminPosting(admin.ModelAdmin):
+    list_display = ('judul_film', 'category', 'post_date')
+
+admin.site.register(Posting, AdminPosting)
--- film/models.py
+++ film/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=155)
+
+    def __str__(self):
+        return self.name
+
+class Posting(models.Model):
+    judul_film = models.CharField(max_length=155)
+    post_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='media')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
