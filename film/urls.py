4d28c5aae25682a541d92a49a8a9db2b89a3811f
soedarhanaINJ <i.soedarhana@gmail.com>
2023-08-27 17:34:40 +0900

    Create func for user

--- film/urls.py
+++ film/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('movies/<int:movie_id>', views.movie, name='movie'),
+    path('movies/upload', views.upload, name='upload'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- film/views.py
+++ film/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import Http404, HttpResponse
 from django.template import loader
+
+from film.forms import UploadForm
 from .models import Movie
 
 def index(request):
-    myposting = Movie.objects.all().values()
+    allmovie = Movie.objects.all().values()
     template = loader.get_template('index.html')
     context = {
-        'myposting': myposting
+        'allmovie': allmovie
     }
 
     return HttpResponse(template.render(context, request))
@@ -19,3 +21,16 @@ def movie(request, movie_id):
         return render(request, 'movie.html', {'movie': movie})
     else:
         return Http404('Movie not FOunds !')
+
+# Functions upload for user can upload with herself    
+def upload(request):
+    if request.POST:
+        form = UploadForm(request.POST, request.FILES)
+        print(request.FILES)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect(index)
+
+    return render(request, 'upload.html', {'form': UploadForm})
